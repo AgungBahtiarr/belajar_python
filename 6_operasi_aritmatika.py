39774486806f2b66d23fab6a6ea75679506fd619
AgungBahtiarr <agungbahtiar050@gmail.com>
2022-08-28 19:23:36 +0700

    Menambahkan materi3

---
+++ 6_operasi_aritmatika.py
@@ -0,0 +1,11 @@
+#operasi + - / * % **=eksponen=pangkat //=floor=pembagian di bulatkan
+#prioritas nya = () * +
+a = 5
+b = 5
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a**b)
+print(a//b)
---
+++ 7_latihan_hitung_sederhana.py
@@ -0,0 +1,20 @@
+#membuat program menghitung luas bangun datar
+#persergi
+sisi=input("Masukkan Panjang Sisi Persegi ")
+sisi=int(sisi)
+print(sisi*sisi)
+
+#persegi panjang
+
+panjang=input("masukkan panjang ")
+lebar=input("masukkkan lebar ")
+panjang=int(panjang)
+lebar=int(lebar)
+print(panjang * lebar)
+
+#Segitiga
+alas=input("masukkan alas segitiga ")
+alas=int(alas)
+tinggi=input("masukkan tinggi segitiga ")
+tinggi=int(tinggi)
+print(1/2 * alas * tinggi)
